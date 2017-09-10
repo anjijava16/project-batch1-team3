bc4156036917f9dd6a3cd1fb2d6751fe20719a7e
srivignesh001 <srivigneshbank@gmail.com>
2017-09-11 00:40:39 +0530

    //testing commit

--- Hadoop_Training project-Srivignesh/src/com/orienit/hadoop/training/WordCountJob.java
+++ Hadoop_Training project-Srivignesh/src/com/orienit/hadoop/training/WordCountJob.java
@@ -1,5 +1,5 @@
 package com.orienit.hadoop.training;
-
+// welcome to hadoop word
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;

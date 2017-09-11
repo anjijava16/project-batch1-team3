78c78a119652ccb45006747b9f05fd466c25a5d7
dinesh-orienit <31597109+dinesh-orienit@users.noreply.github.com>
2017-09-11 11:42:29 +0530

    editing file

--- Hadoop_Training project-Srivignesh/src/com/orienit/hadoop/training/WordCountJob.java
+++ Hadoop_Training project-Srivignesh/src/com/orienit/hadoop/training/WordCountJob.java
@@ -1,5 +1,6 @@
 package com.orienit.hadoop.training;
 // welcome to hadoop word
+// dinesh editing
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
